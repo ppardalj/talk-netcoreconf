bed2044782fd864d7e0e2d1cf93bbfd5a3f67c59
Pedro Pardal <ppardalj@gmail.com>
2023-05-27 10:31:47 +0200

    initial commit

---
+++ trip-service-kata/TripServiceKata.Tests/TripServiceShould.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace TripServiceKata.Tests
+{
+    public class TripServiceShould
+    {
+        [Fact]
+        public void EmptyTest()
+        {
+            Assert.True(true);
+        }
+    }
+}
